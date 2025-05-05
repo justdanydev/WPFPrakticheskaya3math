396502c7b48256c7e0fc2135a1604e759eb6aac4
JUSTDANYdev <bobrovnikovdanila@gmail.com>
2025-05-05 22:47:14 +0300

    Initial commit

---
+++ Wpf_math_practice3/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_math_practice3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int taskNumber = int.Parse(button.Content.ToString().Replace("Задание ", ""));
+
+        }
+    }
+}
